40447bb7c654c0e7483e5160c0f01dde95dc248a
Theo Conzi <theo.a.conzi@gmail.com>
2023-07-13 18:08:15 -0300

    Initial project setup - (controller, dto, repository, service and entity)

--- carProject/src/main/java/br/com/compass/carProject/controller/CarController.java
+++ carProject/src/main/java/br/com/compass/carProject/controller/CarController.java
@@ -1,11 +1,10 @@
 package br.com.compass.carProject.controller;
 
 
+import br.com.compass.carProject.entity.Car;
 import br.com.compass.carProject.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -21,10 +20,15 @@ public class CarController {
 
     }
 
-//	@GetMapping("/post")
-//	public String post() {
-//		return "Dentro do controller";
-//
-//	}
+    @PostMapping("/post")
+    public String post(@RequestBody Car car) {
+        return car.toString();
 
+    }
+
+
+    @PostMapping("/post")
+    public String post(@PathVariable String idChassi) {
+        return idChassi;
+    }
 }
---
+++ carProject/src/test/java/br/com/compass/carProject/CarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.carProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
