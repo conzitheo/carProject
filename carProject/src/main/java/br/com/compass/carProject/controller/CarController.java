9134c3bbf2abea2eb8e34c3898a5ed3a8182b121
Theo Conzi <theo.a.conzi@gmail.com>
2023-07-13 21:45:59 -0300

    Implement POST and GET endpoints

--- carProject/src/main/java/br/com/compass/carProject/controller/CarController.java
+++ carProject/src/main/java/br/com/compass/carProject/controller/CarController.java
@@ -1,34 +1,47 @@
 package br.com.compass.carProject.controller;
 
 
-import br.com.compass.carProject.entity.Car;
+import br.com.compass.carProject.dto.CarDTO;
+import br.com.compass.carProject.entity.CarEntity;
 import br.com.compass.carProject.service.CarService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/cars")
 public class CarController {
 
     @Autowired
     CarService carService;
 
-    @GetMapping("/car")
-    public String car() {
-        return carService.getString();
 
-    }
-
-    @PostMapping("/post")
-    public String post(@RequestBody Car car) {
-        return car.toString();
-
+    public CarController(@NotNull CarService carService) {
+        this.carService = carService;
     }
 
 
+    @GetMapping("/get/{idChassi}")
+    public ResponseEntity<CarEntity> getCar(@PathVariable Long idChassi) {
+        try {
+            CarEntity carEntity = carService.getCarById(idChassi);
+            if (carEntity != null) {
+                return ResponseEntity.ok(carEntity);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+
     @PostMapping("/post")
-    public String post(@PathVariable String idChassi) {
-        return idChassi;
+    public ResponseEntity<CarEntity> createCar(@RequestBody CarDTO carDTO) {
+        CarEntity savedCarEntity = carService.saveCar(carDTO);
+        return ResponseEntity.ok(savedCarEntity);
     }
 }
